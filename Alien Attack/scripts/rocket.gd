a83630352de46831f90ab7277b3f082d84909ea9
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-09-30 18:48:50 -0500

    delete rocket on exit screen

--- Alien Attack/scripts/rocket.gd
+++ Alien Attack/scripts/rocket.gd
@@ -1,6 +1,13 @@
 extends Area2D
 
 @export var rocket_speed = 5
+@onready var onScreenNode = $OnScreenNotifier
+
+func _ready():
+    onScreenNode.connect("screen_exited", _on_screen_exited)
 
 func _physics_process(delta):
     global_position.x += rocket_speed * delta
+
+func _on_screen_exited():
+    queue_free()
