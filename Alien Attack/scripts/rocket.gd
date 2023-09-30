8c32a7502827c8b52d256ff8cbe6c63945d7edc3
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-09-30 17:55:15 -0500

    adjust rocket for FPS

--- Alien Attack/scripts/rocket.gd
+++ Alien Attack/scripts/rocket.gd
@@ -2,5 +2,5 @@ extends Area2D
 
 @export var rocket_speed = 5
 
-func _physics_process(_delta):
-    global_position.x += rocket_speed
+func _physics_process(delta):
+    global_position.x += rocket_speed * delta
