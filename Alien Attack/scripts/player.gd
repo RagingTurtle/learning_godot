a5c21572cf72bf6a64b4fec43651c3f4841ffcbf
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-09-30 18:17:43 -0500

    shoot rocket

--- Alien Attack/scripts/player.gd
+++ Alien Attack/scripts/player.gd
@@ -2,21 +2,32 @@ extends CharacterBody2D
 
 var speed = 300
 
+var rocket_scene = preload("res://scenes/rocket.tscn")
+
+func _process(_delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+
 func _physics_process(_delta):
 
-    velocity = Vector2(0,0)
-        
-    if Input.is_action_pressed("move_down"):
-        velocity.y = speed
-    if Input.is_action_pressed("move_up"):
-        velocity.y = -speed
-    if Input.is_action_pressed("move_left"):
-        velocity.x = -speed
-    if Input.is_action_pressed("move_right"):
-        velocity.x = speed
-    
-    move_and_slide()
-    
-    var viewport_size = get_viewport_rect().size
+	velocity = Vector2(0,0)
+		
+	if Input.is_action_pressed("move_down"):
+		velocity.y = speed
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+	if Input.is_action_pressed("move_right"):
+		velocity.x = speed
+	
+	move_and_slide()
+	
+	var viewport_size = get_viewport_rect().size
 
-    global_position = global_position.clamp(Vector2(0,0), viewport_size)
+	global_position = global_position.clamp(Vector2(0,0), viewport_size)
+	
+func shoot():
+	var rocket_instance = rocket_scene.instantiate()
+	add_child(rocket_instance)
+	rocket_instance.global_position.x += 80
