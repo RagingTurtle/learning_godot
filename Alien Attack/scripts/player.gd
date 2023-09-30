6b6cd74fce9a220fbe7b7c2c183862c13f2c61f0
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-09-30 18:36:27 -0500

    rocket container

--- Alien Attack/scripts/player.gd
+++ Alien Attack/scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var speed = 300
 
 var rocket_scene = preload("res://scenes/rocket.tscn")
+@onready var rocket_container = $RocketContainer
 
 func _process(_delta):
 	if Input.is_action_just_pressed("shoot"):
@@ -29,5 +30,6 @@ func _physics_process(_delta):
 	
 func shoot():
 	var rocket_instance = rocket_scene.instantiate()
-	add_child(rocket_instance)
+	rocket_container.add_child(rocket_instance)
+	rocket_instance.global_position = global_position
 	rocket_instance.global_position.x += 80
