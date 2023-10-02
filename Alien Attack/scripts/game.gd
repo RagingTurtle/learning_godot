33b87c35b6bcdb2d228fd779a62837c4ff2c9eb0
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-10-01 21:27:07 -0500

    game over screen

--- Alien Attack/scripts/game.gd
+++ Alien Attack/scripts/game.gd
@@ -6,20 +6,28 @@ var score = 0
 @onready var player = $Player
 @onready var enemy_container = $EnemySpawner/EnemyContainer
 @onready var hud = $UI/HUD
+@onready var ui = $UI
+
+var game_over_scene = preload("res://scenes/game_over_screen.tscn")
 
 func _ready():
 	hud.set_score_label(score)
 	hud.set_lives(lives)
 
 func _on_deathzone_area_entered(area):
-	area.die()
+	area.queue_free()
 	
 func _on_player_took_damage():
 	lives -= 1
 	hud.set_lives(lives)
 	if (lives == 0):
-		print("Game over")
 		player.die()
+		
+		await get_tree().create_timer(1.5).timeout
+		
+		var game_over_instance = game_over_scene.instantiate()
+		game_over_instance.set_score(score)
+		ui.add_child(game_over_instance)
 		
 func _on_enemy_spawner_enemy_spawned(enemy_instance):
 	enemy_instance.connect("died", _on_enemy_died)
