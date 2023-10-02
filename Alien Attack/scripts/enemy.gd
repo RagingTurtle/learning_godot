40fc31ffc402159c2bd56c4294978970a82a0f3f
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-10-01 20:44:31 -0500

    Lives and basic scoring

--- Alien Attack/scripts/enemy.gd
+++ Alien Attack/scripts/enemy.gd
@@ -1,9 +1,16 @@
 extends Area2D
 
+signal died
+
 @export var enemy_speed = 5
 
 func _physics_process(delta):
 	global_position.x -= enemy_speed * delta
 
 func die():
+	emit_signal("died")
 	queue_free()
+
+func _on_body_entered(body):
+	body.take_damage()
+	die()
--- Alien Attack/scripts/game.gd
+++ Alien Attack/scripts/game.gd
@@ -1,5 +1,26 @@
 extends Node2D
 
+@export var lives = 3
+var score = 0
+
+@onready var player = $Player
+@onready var enemy_container = $EnemySpawner/EnemyContainer
+
 func _on_deathzone_area_entered(area):
 	area.die()
 	
+func _on_player_took_damage():
+	lives -= 1
+	if (lives == 0):
+		print("Game over")
+		player.die()
+	else:
+		print(lives)
+
+func _on_enemy_spawner_enemy_spawned(enemy_instance):
+	enemy_instance.connect("died", _on_enemy_died)
+	enemy_container.add_child(enemy_instance)
+
+func _on_enemy_died():
+	score += 100
+	print("Score: " + str(score))
