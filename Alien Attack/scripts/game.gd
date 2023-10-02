7dc5a53380d49a11e5330465df42e799805c0e35
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-10-01 21:04:21 -0500

    lives ui

--- Alien Attack/scripts/game.gd
+++ Alien Attack/scripts/game.gd
@@ -9,18 +9,18 @@ var score = 0
 
 func _ready():
 	hud.set_score_label(score)
+	hud.set_lives(lives)
 
 func _on_deathzone_area_entered(area):
 	area.die()
 	
 func _on_player_took_damage():
 	lives -= 1
+	hud.set_lives(lives)
 	if (lives == 0):
 		print("Game over")
 		player.die()
-	else:
-		print(lives)
-
+		
 func _on_enemy_spawner_enemy_spawned(enemy_instance):
 	enemy_instance.connect("died", _on_enemy_died)
 	enemy_container.add_child(enemy_instance)
@@ -28,3 +28,4 @@ func _on_enemy_spawner_enemy_spawned(enemy_instance):
 func _on_enemy_died():
 	score += 100
 	hud.set_score_label(score)
+
