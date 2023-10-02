08bc77a9e66fdd5ce0c65ed2c8158b2295f65785
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-10-02 11:59:20 -0500

    player movement

--- Martian Mike/scripts/player.gd
+++ Martian Mike/scripts/player.gd
@@ -1,1 +1,22 @@
 extends CharacterBody2D
+
+@export var gravity = 400
+@export var max_y_velocity = 500
+@export var jump_force = 250
+@export var speed = 125
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	if is_on_floor() == false:
+		velocity.y += gravity * delta
+		if velocity.y > max_y_velocity:
+			velocity.y = max_y_velocity
+	
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_force
+		
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	velocity.x = direction * speed
+	move_and_slide()
