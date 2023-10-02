2c63371bb614465d8f92b2054f9164908c655342
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-10-02 13:22:13 -0500

    stop double jump

--- Martian Mike/scripts/player.gd
+++ Martian Mike/scripts/player.gd
@@ -13,10 +13,9 @@ func _physics_process(delta):
 		if velocity.y > max_y_velocity:
 			velocity.y = max_y_velocity
 	
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -jump_force
 		
-	
 	var direction = Input.get_axis("move_left", "move_right")
 	velocity.x = direction * speed
 	move_and_slide()
