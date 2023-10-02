22c4df541b061d183bfcbfe458dc51459cd29993
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-10-02 11:44:00 -0500

    player animation

--- Martian Mike/scripts/level.gd
+++ Martian Mike/scripts/level.gd
@@ -3,6 +3,5 @@ extends Node2D
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
-		print("escape")
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
