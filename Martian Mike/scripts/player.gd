9c7f935770d1a112510133a7deb3ff60495212bb
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-10-03 10:28:55 -0500

    jump pad

--- Martian Mike/scripts/player.gd
+++ Martian Mike/scripts/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+class_name Player
 
 @export var gravity = 400
 @export var max_y_velocity = 500
@@ -14,8 +15,7 @@ func _physics_process(delta):
 			velocity.y = max_y_velocity
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = -jump_force
-		
+		jump(jump_force)
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction != 0:
 		animated_sprite.flip_h = (direction == -1)
@@ -25,6 +25,9 @@ func _physics_process(delta):
 	
 	update_animations(direction)
 	
+func jump(force):
+			velocity.y = -force
+
 func update_animations(direction):
 	if is_on_floor():
 		if direction == 0:
@@ -37,6 +40,6 @@ func update_animations(direction):
 		else:
 			animated_sprite.play("fall")
 
-func move_to(position):
+func move_to(new_position):
 	velocity = Vector2.ZERO
-	global_position = position
+	global_position = new_position
