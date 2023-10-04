19d00f27cd9a3bc8b9046d3b898b345fa2dce407
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-10-04 10:05:25 -0500

    refactor timer

--- Martian Mike/scripts/level.gd
+++ Martian Mike/scripts/level.gd
@@ -12,8 +12,6 @@ var player = null
 @export var level_time = 5
 
 var timer_node = null
-var time_left
-var ONE_SECOND = 1
 
 var win = false
 
@@ -28,32 +26,28 @@ func _ready():
 	exit.body_entered.connect(_on_exit_body_entered)
 	deathzone.body_entered.connect(_on_deathzone_body_entered)
 	
-	time_left = level_time
-	timer_hud.set_time_label(time_left)
-	
 	timer_node = Timer.new()
 	timer_node.name = "Level Timer"
-	timer_node.wait_time = ONE_SECOND
+	timer_node.wait_time = level_time
 	timer_node.timeout.connect(_on_level_timer_timeout)
 	add_child(timer_node)
 	timer_node.start()
 	
 func _on_level_timer_timeout():
 	if !win:
-		time_left -= ONE_SECOND
-		timer_hud.set_time_label(time_left)
-		print(time_left)
-		if time_left < 0:
-			reset_player()
-			time_left = level_time
-			timer_hud.set_time_label(time_left)
-
+		reset_player()
+		timer_node.start()
 
 func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
+	update_timer_display()
+	
+func update_timer_display():
+	var timer_hud_delay = .5
+	timer_hud.set_time_label(int(timer_node.time_left + timer_hud_delay))
 
 func _on_deathzone_body_entered(_body):
 	reset_player()
@@ -69,6 +63,7 @@ func _on_exit_body_entered(body):
 		exit.animate()
 		player.active = false
 		win = true
+		timer_node.paused = true
 		await get_tree().create_timer(1.5).timeout
 		if next_level != null:
 			get_tree().change_scene_to_packed(next_level)
